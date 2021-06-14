44c6d68d5f5fac638a1a663f11806859b04ca248
Antoni Sieminski <a.m.sieminski@gmail.com>
2021-06-14 15:57:23 +0100

    changed the interactions such that each one has only a single smooth term

--- code/formula_full.R
+++ code/formula_full.R
@@ -12,6 +12,23 @@ mu_f <- faultCount ~
   # Continuous Variables
   bols(wind_gust_max, intercept = FALSE) + 
   bbs(wind_gust_max, center = TRUE, df = 1) +
+  bols(wind_mean, intercept = FALSE) + 
+  bbs(wind_mean, center = TRUE, df = 1) +
+  # Low temperature can lead to icing and higher line density
+  bols(temp_min, intercept = FALSE) +
+  bbs(temp_min,center =TRUE, df = 1) +
+  # Temperature can expand the length of the lines making them more vulnerable
+  bols(temp_max, intercept = FALSE) +
+  bbs(temp_max,center =TRUE, df = 1) +
+  
+  # Not sure how can rain affect the distribution lines
+  bols(rain_min, intercept = FALSE) +
+  bbs(rain_min,center =TRUE, df = 1) +
+  
+  bols(rain_max, intercept = FALSE) +
+  bbs(rain_max,center =TRUE, df = 1) +
+  
+    
   # Continuous Cyclic
   bols(wind_direction, intercept = FALSE) +
   bbs(wind_direction, center = TRUE, df = 1,
@@ -25,28 +42,34 @@ mu_f <- faultCount ~
   bols(regionCode, intercept = FALSE, df = 1) +
   bmrf(regionCode,bnd=nb, center = TRUE, df = 1)
   
-  # -- Two-way interactions --#
-  # Wind_direction X Region
-    # lin-lin
-    bols(wind_direction, by = regionCode, intercept = FALSE) +
-    # smooth-lin
-    bbs(wind_direction, by = regionCode, center = TRUE, df = 1,
-      cyclic = TRUE, boundary.knots = wind_direction_bk) +
-  # Wind_direction X Wind_Gust
-    # lin-lin
-    bols(wind_direction, by = wind_gust_max, intercept = FALSE) + 
-    # smooth-lin
-    bbs(wind_direction, by = wind_gust_max, center = TRUE, df = 1,
+  # Gust X Direction #
+  #bols(wind_gust_max, intercept = FALSE) +  #Already added above
+  #bols(wind_direction, intercept = FALSE) + #Already added above
+  bols(wind_direction, by = wind_gust_max, intercept = FALSE) +
+  bbs(wind_direction, center = TRUE, df = 1, cyclic = TRUE, 
+      boundary.knots = wind_direction_bk) %X% 
+    bbs(wind_gust_max, center = TRUE, df = 1)
+    
+  # Direction X Region
+  #bols(wind_direction, intercept = FALSE) + #Already added above
+  #bols(regionCode, intercept = FALSE, df = 1) + #Already added above
+  bols(wind_direction, by = regionCode, intercept = FALSE, df = 1) +
+  bbs(wind_direction, by = regionCode, center = TRUE, df = 1,
         cyclic = TRUE, boundary.knots = wind_direction_bk) +
-    # lin-smooth
-    bbs(wind_gust_max, by = wind_direction, center = TRUE, df = 1) + 
-    # smooth-smooth
-    bbs(wind_direction, center = TRUE, df = 1, 
-      cyclic = TRUE, boundary.knots = wind_direction_bk
-      ) %X% bbs(wind_gust_max, center = TRUE, df = 1)
-  # Region X Wind_Gust
-    bols()
-    bbs(wind_gust_max, by = wind_direction, center = TRUE, df = 1) + 
+  
+  # Gust X Region
+  #bols(wind_gust_max, intercept = FALSE) +  #Already added above
+  #bols(regionCode, intercept = FALSE, df = 1) + #Already added above
+  bols(wind_gust_max, by = regionCode, intercept = FALSE) +
+  bbs(wind_gust_max, by = regionCode, center = TRUE, df = 1) +
+    
+  # Direction X Gust X Region
+  # All main effects and two-way linear interactions were added above
+  bols(wind_direction, by = wind_gust_max, 
+       intercept = FALSE) %X% bols(regionCode, intercept = FALSE, df = 1) +
+  bbs(wind_direction, center = TRUE, df = 1, cyclic = TRUE, 
+      boundary.knots = wind_direction_bk) %X% 
+    bbs(wind_gust_max, by = regionCode, center = TRUE, df = 1) 
   
     
 sigma_f <- faultCount ~ bols(Intercept, intercept = FALSE) #+ bols(day, intercept = FALSE)

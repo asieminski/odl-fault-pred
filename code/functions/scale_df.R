76e49b5838c0cde842dd040575fe09a414d511a9
Antoni Sieminski <a.m.sieminski@gmail.com>
2021-06-24 18:40:30 +0100

    Change from OOM style scaling to functional due to lack of compatibility with other functions.

--- code/functions/scale_df.R
+++ code/functions/scale_df.R
@@ -1,29 +1,22 @@
-# scale_df()
-# Scales and centres all numeric variables in a dataset 
-# ..except for the ones specified in the omit argument.
+# norm_fit()
+# Finds means and standard deviations for all numeric variables in 
+# ..a dataset except for the ones specified in the "omit" argument.
 # Inputs:
-# use_scaling_from -- a data.frame of class "scale_df", 
-# ..which contains former means and standard deviations of a dataset.
+# df -- a data.frame object with numeric columns
 # omit -- a character vector with varaible names to be unaffected by the funciton
 # na.rm -- logical, specifies if NA values should be removed for the scaling
 #
+# norm_transform()
+# Scales and centres all numeric variables in a dataset.
+# Inputs:
+# df -- a data.frame to be transformed
+# norm_fit -- a named list with two vectors of equal length: "means" and "sds"
+# ..both with variable names as their values
 # Examples
 # df <- readRDS("data/trainingData/trainingData_1_2.rds")
-# 
-# df_sc <- scale_df(df, omit = "faultCount")
-# attributes(df_sc)
-# 
-# scale_df(df, use_scaling_from = df_sc, omit = "rain_min")
-
-scale_df <- function(df, use_scaling_from = NULL, na.rm = TRUE,
-                     omit = NULL){
-  # Check if scale_df was used on the df
-  if("scaled_df" %in% class(df)){
-    stop("The df has already been scaled.")
-    return(df)
-  }
-  # Indicate that the object has been scaled with this function
-  class(df) <- c(class(df), "scaled_df")
+# (nf <- norm_fit(df))
+# (df_sc <- norm_transform(df, nf))
+norm_fit <- function(df, omit = NULL, na.rm = TRUE){
   # Add means and sds for each numeric column to the object
   if(!is.null(omit)){
     if(!all(is.character(omit))){
@@ -31,55 +24,21 @@ scale_df <- function(df, use_scaling_from = NULL, na.rm = TRUE,
     }
     omit_cols <- !(colnames(df) %in% omit)
   } else {
-    omit_cols <- rep(TRUE, ncol(df))
+    omit_cols <- TRUE
   }
-  # Normalise using the provided data.frame
-  if(!is.null(use_scaling_from)){
-    # Check if the data.frame has the right characteristics
-    if(!("scaled_df" %in% class(use_scaling_from))){
-      stop("The supplied data.frame must be of class \"scaled_df\"")
-    }
-    cat("Scaling using the supplied data.frame\n")
-    attr(df, "means") <- attr(use_scaling_from, "means")
-    attr(df, "sds") <- attr(use_scaling_from, "sds")
-  # If no exemplar provided, estimates means and sds
-  } else {
-    num_cols <- which(sapply(df, is.numeric) & omit_cols)
-    ndf <- df[, num_cols]
-    attr(df, "means") <- sapply(ndf, mean, na.rm = na.rm)
-    attr(df, "sds") <- sapply(ndf, sd, na.rm = na.rm)
-  }
+  num_cols <- which(sapply(df, is.numeric) & omit_cols)
+  ndf <- df[, num_cols]
+  means <- sapply(ndf, mean, na.rm = na.rm)
+  sds <- sapply(ndf, sd, na.rm = na.rm)
+  norm_info <- list(means = means, sds = sds)
+  return(norm_info)
+}
+norm_transform <- function(df, norm_fit){
   # Scale and centre each column
-  for(num_var in names(attr(df, "means"))){
-    df[, num_var] <- 
-      (df[, num_var] - attr(df, "means")[num_var])/
-      attr(df, "sds")[num_var]
+  for(var in names(norm_fit$means)){
+    df[, var] <- 
+      (df[, var] - norm_fit$means[var])/
+      norm_fit$sds[var]
   }
   return(df)
-}  
-# Tests:
-# scale_df(df, )
-# means_before <- colMeans(df[, which(sapply(df, is.numeric))])
-# sds_before <- (df[, which(sapply(df, is.numeric))]) |>
-#   lapply(sd)
-# means_after <- colMeans(df[, which(sapply(df, is.numeric))], na.rm = TRUE)
-# all(abs(means_after) < 1e-15)
-# df <- unscale_df(df)
-# colMeans(df[, which(sapply(df, is.numeric))]) - means_before < 1e-16
-
-unscale_df <- function(df){
-  # Check if scale_df was used on the df
-  if(!("scaled_df" %in% class(df))){
-    stop("The df object needs to be created with scale_df()")
-  }
-  # Reverse the scaling and centring
-  scaled_cols <- names(attributes(df)$means)
-  for(col in scaled_cols){
-    df[, col] <-  df[, col] * attributes(df)$sds[col] + 
-      attributes(df)$means[col] 
-  }
-  attr(df, "means") <- NULL
-  attr(df, "sds") <- NULL
-  
-  return(df)
-}  
+}
